f66e1f3d78268f54f02966ec5aeaee6bf85ac29b
reeshmaar <reeshmaar1998@gmail.com>
2017-03-06 14:34:09 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,10 @@
+// my third c program
+#include <stdio.h>
+int main()
+{
+   int a = 10;
+   int b = 5;
+   int c = a/b;
+   printf("the value of c is %d\n",c);
+   return 0;
+}
---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+int a,b;
+printf("Enter two numbers:");
+scanf("%d%d",&a,&b);
+int c=a+b;
+printf("the sum is %d:",c);
+}
